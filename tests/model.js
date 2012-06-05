1dc6b9469374f538c1c409bcf13646213f5a7449
Richard Hodgson <rightaboutnow@gmail.com>
2012-06-05 20:22:45 +0100

    Proxy http client to child modules

--- tests/model.js
+++ tests/model.js
@@ -2,7 +2,7 @@ var litmus = require('litmus');
 
 exports.test = new litmus.Test('Internal clique objects', function () {
     var test = this;
-    test.plan(11);
+    test.plan(12);
 
     var Module = require('../lib/model').Module;
 
@@ -68,32 +68,47 @@ exports.test = new litmus.Test('Internal clique objects', function () {
 
         test.isa(testPackage, Package, 'can create an instance of Package');
 
-        var modules = testPackage.getModules();
-
-        test.is(
-            modules,
-            {
-                'http://example.com/one.js': new Module('http://example.com/one.js'),
-                'http://example.com/two.js': new Module('http://example.com/two.js')
-            },
-            'Package creates module instances for each url is was constructed with'
-        );
 
         var counter = 0,
             mock_request = {};
         mock_request.get = function (url, callback) {
             counter++;
             callback(null, {statusCode: 200}, 'response' + counter);
-        }
+        };
 
-        for (var module in modules) {
-            modules[module].setHttpClient(mock_request);
-        }
+        testPackage.setHttpClient(mock_request);
+
+        var modules = testPackage.getModules();
+
+        test.is(
+            modules,
+            createMockModules(Module, mock_request),
+            'Package creates module instances for each url is was constructed with'
+        );
+
+        test.is(
+            modules['http://example.com/one.js'].getHttpClient().get,
+            mock_request.get,
+            'Package proxies http client to child Modules'
+        );
 
         testPackage.get().then(function (contents) {
             test.is(contents, 'response1 response2', 'Package is created from module responses');
             complete.resolve();
         });
     });
-
 });
+
+function createMockModules (Module, httpClient) {
+
+    var modules = {
+        'http://example.com/one.js': new Module('http://example.com/one.js'),
+        'http://example.com/two.js': new Module('http://example.com/two.js')
+    }
+
+    for (var module in modules) {
+        modules[module].setHttpClient(httpClient);
+    }
+
+    return modules;
+}
