1102723985d19896f9c43ee09ffcdc1c2c1aa99e
Richard Hodgson <rightaboutnow@gmail.com>
2012-06-05 20:04:44 +0100

    Added server.js for running clique webapp outside of proton (e.g. some cloud hosting)

---
+++ server.js
@@ -0,0 +1,27 @@
+/*
+This should really just invoke Proton (use.no.de/proton) however it doesn't
+work with node v6.
+@see https://github.com/usenode/proton.js/issues/5
+*/
+var jsgi     = require('jsgi'),
+    http     = require('http'),
+    WebApp   = require('./lib/webapp').WebApp;
+
+var options = {bindTo: '0.0.0.0', port: 8090}
+
+var webapp = new WebApp();
+
+var server = http.createServer(
+    new jsgi.Listener(function (request) {
+        return webapp.handle(request);
+    })
+);
+
+server.listen(options.port || 80, options.bindTo);
+
+console.log(
+    'Webapp listening on ' +
+    (options.bindTo || '0.0.0.0') +
+    ':' +
+    (options.port || 80)
+);
