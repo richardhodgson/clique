0ac9a33edcfe57112349583cca1dbb1e49c57e1a
Richard Hodgson <rightaboutnow@gmail.com>
2012-06-05 21:10:12 +0100

    Added package endpoint

--- tests/webapp.js
+++ tests/webapp.js
@@ -1,7 +1,10 @@
-var litmus = require('litmus');
+var litmus  = require('litmus'),
+    Promise = require('promised-io/promise').Promise;
 
 exports.test = new litmus.Test('Webapp', function () {
     var test = this;
+
+    test.plan(6);
 
     var WebApp = require('../lib/webapp').WebApp;
 
@@ -15,8 +18,55 @@ exports.test = new litmus.Test('Webapp', function () {
             test.like(response.body[0], /clique/i, 'Page renders something');
             complete.resolve();
         });
-
     });
+
+    test.async('simple package', function (complete) {
+
+        var clique  = new WebApp(),
+            request = new mockRequest(
+                'GET',
+                '/package'
+            );
+
+        request.queryString = 'http%3A%2F%2Fwww.example.com%2Fone.js:http%3A%2F%2Fwww.example.com%2Ftwo.js';
+
+        function MockPackage (urls) {
+            test.is(
+                urls,
+                [
+                    'http://www.example.com/one.js',
+                    'http://www.example.com/two.js'
+                ],
+                'Webapp passes Package class the urls from the querystring'
+            );
+        };
+        MockPackage.prototype.get = function () {
+            var promise = new Promise();
+            promise.resolve('response1 response2');
+            return promise;
+        };
+
+        clique.setPackageClass(MockPackage);
+
+        clique.handle(request).then(function (response) {
+            test.is(response.status, 200, 'Page is found');
+            test.is(response.body[0], 'response1 response2', 'Response renders package');
+            complete.resolve();
+        });
+    });
+
+    var clique = new WebApp();
+
+    test.is(
+        clique.getUrlsFromQuerystring(
+            'http%3A%2F%2Fwww.example.com%2Fone.js:http%3A%2F%2Fwww.example.com%2Ftwo.js'
+        ),
+        [
+            'http://www.example.com/one.js',
+            'http://www.example.com/two.js'
+        ],
+        'Can convert a package querystring into an array of urls'
+    );
 });
 
 
