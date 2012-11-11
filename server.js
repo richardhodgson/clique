17c39212bcf757b9ea9d6e1e44e80a168ceec197
Richard Hodgson <rightaboutnow@gmail.com>
2012-11-11 23:06:02 +0000

    Switched back to standard micro and proton

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 #!/bin/env node
-/*
-This should really just invoke Proton (use.no.de/proton) however it doesn't
-work with node v6.
-@see https://github.com/usenode/proton.js/issues/5
-*/
-var jsgi     = require('jsgi'),
-    http     = require('http'),
+
+var proton   = require('proton'),
     WebApp   = require('./lib/webapp').WebApp;
 
 var options = {
@@ -13,19 +8,4 @@ var options = {
     'port'  : process.env.OPENSHIFT_INTERNAL_PORT || 8090
 };
 
-var webapp = new WebApp();
-
-var server = http.createServer(
-    new jsgi.Listener(function (request) {
-        return webapp.handle(request);
-    })
-);
-
-server.listen(options.port || 80, options.bindTo);
-
-console.log(
-    'Webapp listening on ' +
-    (options.bindTo || '0.0.0.0') +
-    ':' +
-    (options.port || 80)
-);
+proton.run(WebApp, options);
